435c692b282dd7e61cb1abf56afb0428b2c95f29
Ryan Morales <ryan.morales@west-mec.org>
2025-02-25 11:34:34 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ echo "Setting up dotfiles..."
 
 # Create symlinks for dotfiles
 ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.vimrc ~/.vimrc
-ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.tmux.conf ~/.tmux.conf
 
 # Install Vim-Plug
 echo "Installing Vim-Plug..."
@@ -13,13 +12,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # Install Vim plugins non-interactively
 echo "Installing Vim plugins..."
 vim -E -s -u ~/.vimrc +PlugInstall +qall
-
-# Install Tmux Plugin Manager
-echo "Installing Tmux Plugin Manager..."
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 2>/dev/null || (cd ~/.tmux/plugins/tpm && git pull)
-
-# Source tmux configuration
-echo "Sourcing tmux configuration..."
-tmux source-file ~/.tmux.conf 2>/dev/null || true
 
 echo "Dotfiles setup completed!"
